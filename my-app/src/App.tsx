f211a6d6cdb3316a2765fa340fb5624852429bc7
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-01-17 13:19:36 -0600

    added remaining components to app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import TimeframeSelector from './TimeframeSelector';
+import BestPerformers from './BestPerformers';
+import SelectedAssets from './SelectedAssets';
+import GraphDisplay from './GraphDisplay';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <TimeframeSelector />
+      <BestPerformers />
+      <SelectedAssets />
+      <GraphDisplay />
     </div>
   );
 };
