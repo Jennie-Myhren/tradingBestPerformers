0779bd6ea7f0bdd999470f3ded9c647473f215e9
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-01-17 17:20:24 -0600

    mini test for timeframeSelector render

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import TimeframeSelector from './TimeframeSelector';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders timeframe selector', () => {
+  const { getByText } = render(<TimeframeSelector />);
+  const dropdown = getByText(/Past 60 Days/i);
+  expect(dropdown).toBeInTheDocument();
 });
