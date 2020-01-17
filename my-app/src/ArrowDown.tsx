d99650b81cdc2bdfbc531f3ffc1bdd2d57376232
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-01-17 17:10:43 -0600

    working on arrowDown + arrowUp for dropdown

---
+++ my-app/src/ArrowDown.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const ArrowDown: React.FC = () => {
+  return (
+    // <?xml version="1.0" encoding="UTF-8"?>
+    <svg
+      width="18px"
+      height="10px"
+      viewBox="0 0 18 10"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      {/* <!-- Generator: Sketch 52.6 (67491) - http://www.bohemiancoding.com/sketch --> */}
+      <title>Path 2</title>
+      <desc>Created with Sketch.</desc>
+      <g
+        id="Page-1"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+      >
+        <g
+          id="1"
+          transform="translate(-359.000000, -29.000000)"
+          fillRule="nonzero"
+          stroke="#4887DA"
+          strokeWidth="3"
+        >
+          <polyline id="Path-2" points="361 31 368 37 375 31"></polyline>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default ArrowDown;
---
+++ my-app/src/ArrowUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ArrowUp: React.FC = () => {
+  return <h6>UpArrow</h6>;
+};
+
+export default ArrowUp;
