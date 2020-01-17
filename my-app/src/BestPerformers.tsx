4f0002944e9c47f87c9b4777da1abfd2bd14cdd5
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-01-17 10:44:52 -0600

    v. roughly mocked up component; nothing renders

---
+++ my-app/src/BestPerformers.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const BestPerformers: React.FC = () => {
+  return (
+    //keeping same css style as App for now
+    <div className="App">
+      <h2>Best Performers TKTKTKT</h2>
+    </div>
+  );
+};
+
+export default BestPerformers;
---
+++ my-app/src/GraphDisplay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const GraphDisplay: React.FC = () => {
+  return (
+    //keeping same css style as App for now
+    <div className="App">
+      <h2>GraphDisplay TKTKTKT</h2>
+    </div>
+  );
+};
+
+export default GraphDisplay;
