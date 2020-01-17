5a711d51a85db6aeb8b239cfcb6ee9ddd149880f
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2020-01-17 10:45:16 -0600

    Time frame dropdown renders with clock icon

---
+++ my-app/src/TimeframeSelector.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import TimeIcon from './TimeIcon';
+import ArrowDown from './ArrowDown';
+import ArrowUp from './ArrowUp';
+import './TimeSelector.css';
+
+const TimeframeSelector: React.FC = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isSelected, setIsSelected] = useState('');
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const handleSelect = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setIsSelected(event.target.value);
+    //thunk for api request for top three performers in that timeframe
+  };
+
+  return (
+    <div className="TimeSelector">
+      <TimeIcon />
+      <h2>TIME FRAME</h2>
+      <select onClick={handleClick} onChange={handleSelect}>
+        <option value="past 60 minutes">
+          60 Minutes {isClicked ? <ArrowUp /> : <ArrowDown />}
+        </option>
+        {/* Blue line */}
+        <option value="past day">Past Day</option>
+        <option value="past week">Past Week</option>
+        <option value="past month">Past Month</option>
+        <option value="past year">Past Year</option>
+      </select>
+    </div>
+  );
+};
+
+export default TimeframeSelector;
